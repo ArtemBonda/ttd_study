5573792ed6f989e8d1a2b199ebc188339de1f8dc
Artem <garribonda@yandex.ru>
2022-07-10 22:27:07 +0300

    build: Реализация TDD метода POST

---
+++ internal/lib/http-server/v3/server.go
@@ -0,0 +1,38 @@
+package v3
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// PlayerStore stores score information about players.
+type PlayerStore interface {
+	GetPlayerScore(name string) string
+}
+
+// PlayerServer is a HTTP interface for player information.
+type PlayerServer struct {
+	Store PlayerStore
+}
+
+func (s *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	score := s.Store.GetPlayerScore(player)
+	if score == "" {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func GetPlayersScore(name string) string {
+	if name == "Pepper" {
+		return "20"
+	}
+
+	if name == "Floyd" {
+		return "10"
+	}
+	return ""
+}
---
+++ internal/lib/http-server/v3/server_test.go
@@ -0,0 +1,87 @@
+package v3
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayer(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]string{
+			"Pepper": "20",
+			"Floyd":  "10",
+		},
+	}
+
+	server := &PlayerServer{
+		Store: &store}
+
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		got := response.Body.String()
+		want := "20"
+
+		assertStatusCode(t, response.Code, http.StatusOK)
+		assertResponseBody(t, got, want)
+	})
+
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		assertStatusCode(t, response.Code, http.StatusOK)
+		assertResponseBody(t, got, want)
+	})
+
+	t.Run("Возврат кода 404 для отсутствующих игроков", func(t *testing.T) {
+		request := newGetScoreRequest("Apple")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := response.Code
+		want := http.StatusNotFound
+
+		assertStatusCode(t, got, want)
+		assertResponseBody(t, response.Body.String(), "")
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Response body is wrong, got %q, want %q", got, want)
+	}
+}
+
+func assertStatusCode(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("status code: got %d, want %d", got, want)
+	}
+
+}
+
+type StubPlayerStore struct {
+	scores map[string]string
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) string {
+	score := s.scores[name]
+	return score
+}
