58ba678dc42c0af58ae7a231aa5853ff1655374b
Artem <garribonda@yandex.ru>
2022-07-10 07:48:27 +0300

    refactor<test>: Добавил тест проверки результата

--- internal/lib/http-server/v1/server_test.go
+++ internal/lib/http-server/v1/server_test.go
@@ -7,14 +7,28 @@ import (
 )
 
 func TestGETPlayer(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/players/Papper", nil)
-	response := httptest.NewRecorder()
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Papper", nil)
+		response := httptest.NewRecorder()
 
-	PlayServer(response, request)
-	got := response.Body.String()
-	want := "20"
+		PlayServer(response, request)
+		got := response.Body.String()
+		want := "20"
 
-	if got != want {
-		t.Errorf("got %s, want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayServer(response, request)
+
+		got := response.Body.String()
+		want := "30"
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
