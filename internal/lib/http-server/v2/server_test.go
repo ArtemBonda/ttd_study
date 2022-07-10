0ec15a76063a519b027635ff0ea675ad66bc2dbd
Artem <garribonda@yandex.ru>
2022-07-10 18:41:27 +0300

    refactor: Добавил 2 хелпера в тест

--- internal/lib/http-server/v2/server_test.go
+++ internal/lib/http-server/v2/server_test.go
@@ -1,6 +1,7 @@
 package v1
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,28 +9,36 @@ import (
 
 func TestGETPlayer(t *testing.T) {
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
 		PlayServer(response, request)
 		got := response.Body.String()
 		want := "20"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
 	t.Run("returns Floyd's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		request := newGetScoreRequest("Floyd")
 		response := httptest.NewRecorder()
 
 		PlayServer(response, request)
 
 		got := response.Body.String()
-		want := "30"
-		
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		want := "10"
+
+		assertResponseBody(t, got, want)
 	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("Response body is wrong, got %q, want %q", got, want)
+	}
 }
